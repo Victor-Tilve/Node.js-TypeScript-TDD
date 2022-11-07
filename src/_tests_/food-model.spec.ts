5cffaa1fdabceb8757d3aa875189af07cb843db6
Victor-Tilve <ing.victortilve@gmail.com>
2022-11-06 19:26:54 -0500

    refator: add FoodHelper inject constructor FoodModel

--- src/_tests_/food-model.spec.ts
+++ src/_tests_/food-model.spec.ts
@@ -2,8 +2,12 @@ import { FoodModel } from '../models/food-model'
 
 describe('Testing Food Model', () => {
   it('Class FoodModel', () => {
-    const foodModel = new FoodModel('Lechuga',
-      'Una verdura saludable', 3000)
+    const foodModel = new FoodModel(
+      {
+        name: 'Lechuga',
+        description: 'Una verdura saludable',
+        price: 3000
+      })
     expect(foodModel.getName()).toEqual('Lechuga')
     expect(foodModel.getDescription()).toEqual('Una verdura saludable')
     expect(foodModel.getPrice()).toBe(3000)

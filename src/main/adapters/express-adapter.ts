629875aa13c181478988c4d38ff5af390281a399
Victor-Tilve <ing.victortilve@gmail.com>
2022-11-08 17:56:59 -0500

    feat: expess adapter

---
+++ src/main/adapters/express-adapter.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express'
+import { Controller } from '../../interfaces/controller-interface'
+import { HttpRequest } from '../../interfaces/http-interface'
+
+export const AdapterRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}

de83a14fe8445169837233db1ea8e14e5b82560e
Victor-Tilve <ing.victortilve@gmail.com>
2022-11-07 23:10:59 -0500

    chore: separating unit and integration tests in Jest

---
+++ jest.config.int.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  verbose: true,
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/***/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  modulePathIgnorePatterns: ['./src/interfaces'],
+  moduleDirectories: ['node_modules'],
+  testMatch: ['**/*test.ts']
+}
+
+export default config
